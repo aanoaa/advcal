12db5e27935532083945b2805d4794a75c6c9330
Hyungsuk Hong <aanoaa@gmail.com>
2015-11-19 18:36:05 +0900

    Add sample doc

---
+++ docs/assets/js/src/docs.js
@@ -0,0 +1,3 @@
+anchors.options.placement = 'left';
+
+anchors.add('.advcal-header > .container > h1, .advcal-container > .row > div > h1, .advcal-container > .row > div > h2, .advcal-container > .row > div > h3, .advcal-container > .row > div > h4, .advcal-container > .row > div > h5');
